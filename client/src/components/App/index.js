b244f7771b075aabefea6eec4e4f55f6279d76b2
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-14 18:20:23 -0500

    create sample planner components

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -6,9 +6,45 @@ import initialData from '../sampleData';
 
 class App extends React.Component {
   state  = initialData;
-  
+ 
+  // onDragStart = () => {
+  //   document.body.style.color = 'orange';
+  //   document.body.style.transition = 'background-color 0.2s ease';
+  // }
+
+  // onDragUpdate = update => {
+  //   const {destination} = update;
+  //   const opacity = destination ? destination.index / Object.keys(this.state.tasks).length : 0;
+  //   document.body.style.backgroundColor = `rgba(153,241,217, ${opacity})`;
+  // }
+
   onDragEnd = result => {
-    // Todo
+    // document.body.style.color = 'inherit';
+    // document.body.style.backgroundColor = 'inherit';
+    const {destination, source, draggableId} = result;
+    // End function if no destination
+    if (!destination) return;
+    // End function if destination is the same as origin
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
   render() {
     // return (
@@ -18,6 +54,7 @@ class App extends React.Component {
     // );
     return (
       <DragDropContext onDragEnd={this.onDragEnd} >
+      {/* <DragDropContext onDragEnd={this.onDragEnd} onDragStart={this.onDragStart} onDragUpdate={this.onDragUpdate}> */}
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
