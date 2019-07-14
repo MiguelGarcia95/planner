6f24a0c0fe4a2bc7dd155f354fb31fb1fa5e16a9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-13 22:00:48 -0500

    setup db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+request('./db/mongoose');
 
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
