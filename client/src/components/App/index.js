584ef3417358027965d4ed392de0b8b86be6e7d6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-14 12:46:07 -0500

    create sample planner components

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Column from './column';
 import './styles.css';
+import initialData from '../sampleData';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  state  = initialData;
+  
+  render() {
+    // return (
+    //   <div className="App">
+
+    //   </div>
+    // );
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
 }
 
 export default App;
