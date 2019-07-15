c126e6fd00df0c16f79e9046a15376d2e6c3f4d6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-15 02:27:24 -0500

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route, withRouter} from 'react-router-dom';
+import {Provider, connect} from 'react-redux';
 import '@atlaskit/css-reset';
 import './index.css';
 // import App from './components/App';
 import App from './components';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </BrowserRouter>
+  )
+};
+
+const RootWithAuth = withRouter(connect()(Root));
+
+const RootWithRouter = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <RootWithAuth />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(<RootWithRouter />, document.getElementById('root'));
