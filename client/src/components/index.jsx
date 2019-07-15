1663a8f423e90df0d7bcf6389307751ed5510de1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-15 00:00:37 -0500

    create index page

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/* 
+  Location: Index/home page
+  Purpose: Show current boards
+  Access: Need to be Authenticated *Ignore for now*
+  Features: Create new boards,
+*/ 
+
 class App extends React.Component {
   render() {
     return (
