3d2fd3dfa64ba361f6412714f6ca608ca0145d2a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-15 10:17:02 -0500

    create main page

---
+++ client/src/components/layout/Column/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* 
+  Location: /boardID/boardName page
+  Purpose: Show tasks
+  Access: Need to be Authenticated & creator of board *Ignore for now*
+  Features: Create new tasks
+*/ 
+
+class Column extends React.Component {
+  render() {
+    return (
+      <section className="app">
+
+      </section>
+    )
+  }
+}
+
+export default Column;
---
+++ client/src/components/layout/Task/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* 
+  Location: /boardID/boardName page
+  Purpose: Show Task
+  Access: Need to be Authenticated & creator of board *Ignore for now*
+  Features: Delete tasks
+*/ 
+
+class Task extends React.Component {
+  render() {
+    return (
+      <section className="app">
+
+      </section>
+    )
+  }
+}
+
+export default Task;
