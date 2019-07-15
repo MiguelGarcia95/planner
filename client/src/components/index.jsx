79cf2d8816fb03b89702b42dba2285b9d9a31590
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-14 22:34:56 -0500

    create index page

---
+++ client/src/components/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <section className="app">
+
+      </section>
+    )
+  }
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
 import './index.css';
-import App from './components/App';
+// import App from './components/App';
+import App from './components';
 
 ReactDOM.render(<App />, document.getElementById('root'));
