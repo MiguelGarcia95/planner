7b2348d2ba218f83c7080ce83aaddbfdfc5d94bf
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-15 21:30:31 -0500

    create navbar

--- client/src/components/layout/Navbar/index.jsx
+++ client/src/components/layout/Navbar/index.jsx
@@ -3,23 +3,49 @@ import styled from 'styled-components';
 
 const Container = styled.section`
   height: 50px;
-  background: red;
   display: flex;
+  width: 100%;
   flex: 1;
+  position: fixed;
 `;
 
 const Logo = styled.section`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  .logo {
+    line-height: 50px;
+    height: 50px;
+    width: 50px;
+    text-align: center;
+    font-size: 2em;
+    background: green;
+  }
 `;
 
 const Nav = styled.section`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  .nav {
+    height: 50px;
+    width: 250px;
+    background: green;
+  }
 `;
 
 class Navbar extends React.Component {
   render() {
     return (
       <Container>
-        <Logo></Logo>
-        <Nav></Nav>
+        <Logo>
+          <section className="logo">L</section>
+        </Logo>
+        <Nav>
+          <section className="nav">
+            
+          </section>
+        </Nav>
       </Container>
     )
   }
