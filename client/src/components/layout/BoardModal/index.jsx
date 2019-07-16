7ae03e9b2d7d11a2a3b516ba3e6b12edeae754a3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-16 00:13:16 -0500

    create modals

--- client/src/components/layout/BoardModal/index.jsx
+++ client/src/components/layout/BoardModal/index.jsx
@@ -5,11 +5,7 @@ const Container = styled.section`
   width: 100vw;
   height: 0px;
   overflow: hidden;
-  background: rgba(0,0,0,0.5);
   position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   transition: height 0.5s;
   ${props => props.isOpened && `
     height: 100vh;
@@ -17,15 +13,24 @@ const Container = styled.section`
 `;
 
 const Modal = styled.section`
-  flex-basis: 50%;
+  width: 50%;
   height: 500px;
   background: white;
+  position: absolute;
+  z-index: 2;
+`;
+
+const ModalBackground = styled.section`
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: absolute;
 `;
 
 function BoardModal({isOpen, toggle}) {
   return (
     <Container isOpened={isOpen} >
-      <p onClick={() => toggle()} >X</p>
+      <ModalBackground  onClick={() => toggle()} />
       <Modal></Modal>
     </Container>
   )
--- client/src/components/layout/SettingsModal/index.jsx
+++ client/src/components/layout/SettingsModal/index.jsx
@@ -17,15 +17,25 @@ const Container = styled.section`
 `;
 
 const Modal = styled.section`
-  flex-basis: 50%;
+  width: 50%;
   height: 500px;
   background: white;
+  position: absolute;
+  z-index: 2;
 `;
+
+const ModalBackground = styled.section`
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: absolute;
+`;
+
 
 function SettingsModal({isOpen, toggle}) {
   return (
     <Container isOpened={isOpen} >
-      <p onClick={() => toggle()} >X</p>
+      <ModalBackground  onClick={() => toggle()} />
       <Modal></Modal>
     </Container>
   )
