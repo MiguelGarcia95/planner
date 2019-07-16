067b8c110a1164b4fea397fc03d4f6337898a48b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-16 00:13:42 -0500

    create modals

--- client/src/components/layout/BoardModal/index.jsx
+++ client/src/components/layout/BoardModal/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.section`
-  width: 100vw;
-  height: 0px;
-  overflow: hidden;
-  position: fixed;
-  transition: height 0.5s;
+    width: 100vw;
+    height: 0px;
+    overflow: hidden;
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: height 0.5s;
   ${props => props.isOpened && `
     height: 100vh;
   `}
