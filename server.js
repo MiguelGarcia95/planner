eaa663a7fd1771ab307ef02e0bc52620f379bd93
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-17 03:09:12 -0500

    create database models

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('./db/mongoose');
 
+/*
+  userID: {
+    userId,
+    userName,
+    backgroundColor,
+    navColor,
+    boards: [
+      {
+        boardId: {
+          tasks: {}
+          columns: {},
+          columnOrder: []
+        }
+      }
+    ]
+  }
+*/
+
+/**
+ * Models
+ * UserModel - id, name, email, pswd bgColor, navColor, boards
+ * BoardModel - id, title, description, userId, bgColor, tasks, columns, columnOrder users?
+ * ColumnModel - id, name, taskIds, status, dateStarted, dateCompleted
+ * TaskModel - id, name, description, status, dateStarted, dateCompleted,
+ * 
+ */
+
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
 }) 
