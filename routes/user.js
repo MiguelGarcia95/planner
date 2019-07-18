17dc9d55c0c66cbb2dab8f69c2de46571f56e8a8
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-17 21:46:11 -0500

    create user router

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const user_controller = require('../controllers/user');
+
+router.post('/', user_controller.register);
+router.post('/login', user_controller.login);
+router.post('/logout', user_controller.logout);
+router.post('/logoutAll', user_controller.logoutAll);
