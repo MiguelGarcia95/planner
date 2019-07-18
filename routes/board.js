21c7416cc9d124af8290cb7a6ce79277f8b51962
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-17 21:22:43 -0500

    create board router

---
+++ routes/board.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const board_controller = require('../controllers/board');
+
+router.get('/', board_controller.index);
+router.post('/', board_controller.create);
+router.get('/:id', board_controller.show);
+
+module.exports = router;
