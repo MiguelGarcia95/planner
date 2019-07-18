a475157d2f7f61c4905dfc066983ddbaecd3f4e6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-17 19:46:22 -0500

    create Column model

---
+++ models/Column.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const columnSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  boardId: {
+    type: String,
+    required: true,
+  },
+  taskOrder: {
+    type: Array,
+    required: false,
+  },
+});
+
+const Column = mongoose.model('Board', columnSchema);
+
+module.exports = Column;
