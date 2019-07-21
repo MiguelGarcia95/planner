45f99b16370093c4e2c03a04c4657f19a8c2cc4b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-20 22:51:29 -0500

    rearrange column order

--- client/src/components/Board/index.jsx
+++ client/src/components/Board/index.jsx
@@ -39,36 +39,12 @@ class InnerList extends React.PureComponent {
 
 class Board extends React.Component {
   state = {
-    // state.task.tasks
-    // getTasks(columnId)
     tasks: {
       'task-1': {id: 'task-1', content: 'Take out the trash'},
       'task-2': {id: 'task-2', content: 'Watch the new episode'},
       'task-3': {id: 'task-3', content: 'Shitpost'},
       'task-4': {id: 'task-4', content: 'Snipe noobs on csgo'},
     },
-    // state.column.columns
-    // getColumns(boardId)
-    columns: {
-      'column-1': {
-        id: 'column-1',
-        title: 'To Do',
-        taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
-      },
-      'column-2': {
-        id: 'column-2',
-        title: 'In progress',
-        taskIds: [],
-      },
-      'column-3': {
-        id: 'column-3',
-        title: 'Done',
-        taskIds: [],
-      },
-    },
-    // state.column.columnOrder
-    // getColumnOrder(boardId);
-    columnOrder: ['column-1', 'column-2', 'column-3'],
   };
 
   componentWillMount() {
