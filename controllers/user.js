5e4660913b4b0f71174475a8d8708421533c980f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-23 18:46:43 -0500

    create auth functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 /**
  * Try this out later
@@ -22,8 +23,15 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    
+    const user = await User.findOne({email: req.body.email});
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      res.status(200).send(user);
+    } else {
+      res.status(401).send('Authentication error');  
+    }
   } catch (error) {
+    console.log(error);
     res.status(400).send({error});  
   }
 }
