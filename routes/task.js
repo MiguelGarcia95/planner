dae91843a25a3392756430dfcec04f1563b79ab3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-28 03:43:25 -0500

    clean and style app

--- routes/task.js
+++ routes/task.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const task_controller = require('../controllers/task');
 
-router.get('/', task_controller.index);
-router.post('/', task_controller.create);
+router.get('/', auth.validateToken, task_controller.index);
+router.post('/', auth.validateToken, task_controller.create);
+router.delete('/', auth.validateToken, task_controller.delete);
 
 module.exports = router;
