97af5f1b4320fc0b680e7bc420c4c453af4c4818
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-31 19:54:24 -0500

    delete board and all columns and tasks

--- controllers/board.js
+++ controllers/board.js
@@ -1,59 +1,60 @@
 const Board = require('../models/Board');
 
-exports.index = async (req, res) => {
-  try {
-    const boards = await Board.find({'userId': req.query.userId});
-    res.status(201).send({boards});
-  } catch (error) {
-    res.status(400).send({error});
-  }
-}
-
-exports.create = async (req, res) => {
-  const board = new Board(req.body);
-  try {
-    await board.save();
-    res.status(201).send({board});
-  } catch (error) {
-    console.log(error)
-    res.status(400).send({error});
-  }
-}
-
-exports.show = async (req, res) => {
-  try {
-    const board = await Board.findOne({"_id": req.query.id, 'userId': req.query.userId});
-    res.status(201).send(board);
-  } catch (error) {
-    res.status(400).send({error});
-  }
-}
-
-exports.updateBoardColumns = async (req, res) => {
-  try {
-    const board = await Board.findByIdAndUpdate(req.body._id, {$set:req.body});
-    res.status(201).send(board);
-  } catch (error) {
-    res.status(400).send({error});    
-  }
-}
-
-exports.rearrangeBoardColumns = async (req, res) => {
-  try {
-    const board = await Board.findByIdAndUpdate(req.body._id, {$set:req.body});
-    res.status(201).send(board);
-  } catch (error) {
-    res.status(400).send({error});    
-  }
-}
-
-exports.delete = async (req, res) => {
-  try {
-    // await Board.findByIdAndDelete(req.query.id);
-    const board = await Board.findById(req.query.id);
-    await board.delete();
-    res.status(200).send(board);
-  } catch (error) {
-    res.status(400).send({error});    
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const boards = await Board.find({'userId': req.query.userId});
+      res.status(201).send({boards});
+    } catch (error) {
+      res.status(400).send({error});
+    }
+  },
+  
+  create: async (req, res) => {
+    const board = new Board(req.body);
+    try {
+      await board.save();
+      res.status(201).send({board});
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({error});
+    }
+  },
+  
+  show: async (req, res) => {
+    try {
+      const board = await Board.findOne({"_id": req.query.id, 'userId': req.query.userId});
+      res.status(201).send(board);
+    } catch (error) {
+      res.status(400).send({error});
+    }
+  },
+  
+  updateBoardColumns: async (req, res) => {
+    try {
+      const board = await Board.findByIdAndUpdate(req.body._id, {$set:req.body});
+      res.status(201).send(board);
+    } catch (error) {
+      res.status(400).send({error});    
+    }
+  },
+  
+  rearrangeBoardColumns: async (req, res) => {
+    try {
+      const board = await Board.findByIdAndUpdate(req.body._id, {$set:req.body});
+      res.status(201).send(board);
+    } catch (error) {
+      res.status(400).send({error});    
+    }
+  },
+  
+  delete: async (req, res) => {
+    try {
+      const board = await Board.findById(req.query.id);
+      await board.delete();
+      res.status(200).send(board);
+    } catch (error) {
+      res.status(400).send({error});    
+    }
   }
 }
--- controllers/column.js
+++ controllers/column.js
@@ -1,56 +1,59 @@
 const Column = require('../models/Column');
 
-exports.index = async (req, res) => {
-  try {
-    const columns = await Column.find({'boardId': req.query.boardId});
-    res.status(201).send(columns);
-  } catch (error) {
-    res.status(400).send({error});
-  }
-}
+module.exports = {
 
-exports.create = async (req, res) => {
-  const column = new Column(req.body);
-  try {
-    await column.save();
-    res.status(201).send(column);
-  } catch (error) {
-    res.status(400).send({error});
-  }
-}
+  index: async (req, res) => {
+    try {
+      const columns = await Column.find({'boardId': req.query.boardId});
+      res.status(201).send(columns);
+    } catch (error) {
+      res.status(400).send({error});
+    }
+  },
+  
+  create: async (req, res) => {
+    const column = new Column(req.body);
+    try {
+      await column.save();
+      res.status(201).send(column);
+    } catch (error) {
+      res.status(400).send({error});
+    }
+  },
+  
+  updateColumn: async (req, res) => {
+    try {
+      const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
+      res.status(201).send({column});
+    } catch (error) {
+      res.status(400).send({error});
+    }
+  },
+  
+  updateColumnTasks: async (req, res) => {
+    try {
+      const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
+      res.status(201).send(column);
+    } catch (error) {
+      res.status(400).send({error});    
+    }
+  },
 
-exports.updateColumn = async (req, res) => {
-  try {
-    console.log(req.body);
-    const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
-    res.status(201).send({column});
-  } catch (error) {
-    res.status(400).send({error});
-  }
-}
-
-exports.updateColumnTasks = async (req, res) => {
-  try {
-    const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
-    res.status(201).send(column);
-  } catch (error) {
-    res.status(400).send({error});    
-  }
-}
-exports.rearrangeColumnTasks = async (req, res) => {
-  try {
-    const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
-    res.status(201).send(column);
-  } catch (error) {
-    res.status(400).send({error});    
-  }
-}
-
-exports.delete = async (req, res) => {
-  try {
-    await Column.findByIdAndRemove(req.query.columnId);
-    res.status(201).send('ok');
-  } catch (error) {
-    res.status(400).send({error});    
-  }
+  rearrangeColumnTasks: async (req, res) => {
+    try {
+      const column = await Column.findByIdAndUpdate(req.body._id, {$set:req.body});
+      res.status(201).send(column);
+    } catch (error) {
+      res.status(400).send({error});    
+    }
+  },
+  
+  delete: async (req, res) => {
+    try {
+      await Column.findByIdAndRemove(req.query.columnId);
+      res.status(201).send('ok');
+    } catch (error) {
+      res.status(400).send({error});    
+    }
+  },
 }
