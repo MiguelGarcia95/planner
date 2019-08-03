660e39d2b10e0f68412632a929295c72aa406bcf
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-02 20:04:09 -0500

    clean controllers and actions

--- server.js
+++ server.js
@@ -16,56 +16,6 @@ app.use('/columns', column);
 app.use('/tasks', task);
 app.use('/auth', user);
 
-/*
-  userID: {
-    userId,
-    userName,
-    backgroundColor,
-    navColor,
-    boards: [
-      {id: 'boardId', title: 'titleName },
-      {id: 'boardId2', title: 'titleName2 },
-    ]
-  }
-
-  boardID: {
-    boardId,
-    boardName,
-    userId,
-    tasks: [ MAY NOT NEED
-      {id: taskId, name: 'taskName' ... }
-    ]
-    columns: [ MAY NOT NEED
-      {id: columnId, name: 'columnName' ... }
-    ],
-    columnOrder: ['boardId, boardId2 ...']
-  }
-
-  columnID: {
-    columnId,
-    columnName,
-    boardId,
-    taskOrder: ['taskId, taskId2 ...']
-  }
-
-  taskId: {
-    taskId,
-    columnId,
-    boardId,
-    taskName,
-    taskDescription,
-    ...
-  }
-*/
-
-/**
- * Models
- * UserModel - id, name, email, pswd bgColor, navColor, boards
- * BoardModel - id, title, description, userId, bgColor, tasks, columns, columnOrder users?
- * ColumnModel - id, name, taskIds, status, dateStarted, dateCompleted
- * TaskModel - id, name, description, status, dateStarted, dateCompleted,
- */
-
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
 }) 
