701d57afcefa65c4d45a1fd5fc3ee4abff034f2f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-03 02:49:39 -0500

    display boards

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+  app.get('/:id/:name', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
