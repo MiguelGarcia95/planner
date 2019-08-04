99dcfed9619318d6606e171668ee817f9c80331f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-04 04:22:40 -0500

    clean Board Modal Form

--- client/src/components/layout/BoardModal/index.jsx
+++ client/src/components/layout/BoardModal/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SliderPicker  } from 'react-color';
-import {Container, ModalBackground, Modal, Form} from './styles';
+import {Container, ModalBackground, Modal, Form, Title, Label, Button} from './styles';
 
 function BoardModal({toggle, onFormChange, onSubmit, onColorChange, state}) {
   return (
@@ -9,13 +9,13 @@ function BoardModal({toggle, onFormChange, onSubmit, onColorChange, state}) {
       <ModalBackground  onClick={() => toggle()} />
       <Modal>
         <Form onSubmit={onSubmit} >
-          <h1>Create New Board</h1>
+          <Title>Create New Board</Title>
           <input type='text' name='board_name' onChange={onFormChange} placeholder='Board Name' value={state.board_name} />
-          <p className='label'>Background Color</p>
+          <Label>Background Color</Label>
           <SliderPicker onChange={e => onColorChange(e.hex, 'board_color')} color={state.board_color}/>
-          <p className='label'>Text Color</p>
+          <Label>Text Color</Label>
           <SliderPicker onChange={e => onColorChange(e.hex, 'text_color')} color={state.text_color}/>
-          <button >Submit</button>
+          <Button>Submit</Button>
         </Form>
       </Modal>
     </Container>
--- client/src/components/layout/BoardModal/styles.js
+++ client/src/components/layout/BoardModal/styles.js
@@ -43,21 +43,10 @@ export const Form = styled.form`
   box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
   text-align: center;
   position: relative;
-  input, button {
-    width: 100%;
-    border: none;
-  }
-
-  h1 {
-    height: 80px;
-    line-height: 80px;
-    font-size: 2em;
-    color: black;
-    font-weight: 300;
-  }
-
   input {
     height: 50px;
+    width: 100%;
+    border: none;
     margin: 0;
     margin-bottom: 10px;
     box-sizing: border-box;
@@ -66,35 +55,39 @@ export const Form = styled.form`
     background-color: transparent;
   }
 
-  .label {
-    text-align: left;
-    color: black;
-    line-height: 50px;
-    font-weight: bold;
-    margin: 0;
-  }
-
   input::placeholder {color: black; opacity: 1;}
   input:-ms-input-placeholder {color: black;}
   input::-ms-input-placeholder {color: black;}
 
-  button {
-    height: 100px;
-    cursor: pointer;
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
-    transition: box-shadow transform 0.5s; 
-    color: black;
-    font-size: 1.5em;
-    background: transparent;
-    position: absolute;
-    width: 90%;
-    bottom: 20px; 
-    left: 0; right: 0;
-    margin: auto;
-  }
+`;
 
-  button:active {
-    box-shadow: 0px 0px 5px 2px rgba(0,0,0,0.5);
-    transform: scale(0.95);
-  }
+export const Title = styled.h1`
+  height: 80px;
+  line-height: 80px;
+  font-size: 2em;
+  color: black;
+  font-weight: 300;
+`;
+
+export const Label = styled.p`
+  text-align: left;
+  color: black;
+  line-height: 50px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+export const Button = styled.button`
+  height: 100px;
+  cursor: pointer;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+  color: black;
+  font-size: 1.5em;
+  background: transparent;
+  position: absolute;
+  width: 90%;
+  bottom: 20px; 
+  left: 0; right: 0;
+  margin: auto;
+  border: none;
 `;
